7ab4cf38a51d7716599779e4e02f278fcdea6962
Pedro Oliveira <phr.oliveira08@gmail.com>
2021-10-11 07:34:15 -0300

    add router files

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express = require('express')
 const app = express()
-
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Fullstack Master')
+const indexRouter = require('./routes/index')
+const clientsRouter = require('./routes/clients')
+
+app.use((req, res, next) => {
+  console.log('Passando por todas requisições')
+  next()
 })
+
+app.use('/', indexRouter)
+app.use('/clients', clientsRouter)
 
 app.listen(port, () => {
   console.log('Rodando na porta ', port)
